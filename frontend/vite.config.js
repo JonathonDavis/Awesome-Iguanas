e437c46339f58352c21fd578682c2b7e5d72d319
Jonathon Davis <Jonathonh.davis@gmail.com>
2025-03-30 17:44:04 -0500

    Revert "Vercel Patch"
    This reverts commit bf28dac2c5a6a730983f183ba12829770d557da0.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,18 +4,4 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  build: {
-    sourcemap: true,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          'd3': ['d3'],
-          'vis-network': ['vis-network']
-        }
-      }
-    }
-  },
-  define: {
-    'process.env': {}
-  }
 })
