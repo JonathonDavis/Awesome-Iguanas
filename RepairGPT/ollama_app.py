c5973e56cd71f10b71aa3b7dfa358eb783562cf8
alexander james <pacificocean1912@gmail.com>
2025-04-25 11:53:17 -0500

    Update ollama_app.py

--- RepairGPT/ollama_app.py
+++ RepairGPT/ollama_app.py
@@ -681,6 +681,7 @@ class Neo4jSecurityAnalyzer:
             print('4')  
             
             self.logger.debug("Determining exploitation likelihood")
+            print(vuln_type,"vul type",references,'ref')
             exploitation_likelihood = self._determine_exploitation_likelihood(vuln_type, references)
             self.logger.debug(f"Exploitation likelihood: {exploitation_likelihood}")
             print("5")
