f97b9583467522c6e56d2432e7164e1aa691e2ed
Jonathon Davis <Jonathonh.davis@gmail.com>
2025-05-09 15:42:04 -0500

    Revert "Update neo4jService.js"
    This reverts commit b55d8081cf7dcb96faff8e0f579ce5e44bed12e3.

--- frontend/src/services/neo4j/neo4jService.js
+++ frontend/src/services/neo4j/neo4jService.js
@@ -21,11 +21,7 @@ class Neo4jService {
     
     this.driver = neo4j.driver(
       this.uri,
-      neo4j.auth.basic(this.user, this.password),
-      {
-        encrypted: 'ENCRYPTION_ON',
-        trust: 'TRUST_ALL_CERTIFICATES'
-      }
+      neo4j.auth.basic(this.user, this.password)
     )
     console.log('Neo4j connection established')
     
